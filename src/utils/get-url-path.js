03df2b2053a47b1b38bdf93ee9a8296be1b42213
Tyler Barnes <tylerdbarnes@gmail.com>
2020-04-21 16:26:57 -0700

    use uri instead of getUrlPath helper, still need normalizePath for now

--- src/utils/get-url-path.js
+++ src/utils/get-url-path.js
@@ -1,7 +1,7 @@
-import parsePath from 'parse-path'
+// @todo once the source plugin is updated to the latest WPGQL version, we wont need this helper anymore
 export const normalizePath = path => {
-  if (path.endsWith(`/`)) {
-    path = path.slice(0, -1)
+  if (!path.endsWith(`/`)) {
+    path = `${path}/`
   }
 
   if (!path.startsWith(`/`)) {
@@ -9,15 +9,4 @@ export const normalizePath = path => {
   }
 
   return path
-}
-
-/**
- * This is temporary until we can get a path field from MenuItems https://github.com/wp-graphql/wp-graphql/issues/1137
- *
- * https://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
- */
-export const getUrlPath = url => {
-  const parsedUrl = parsePath(url)
-
-  return normalizePath(parsedUrl.pathname)
 }
