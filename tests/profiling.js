152b658112b65d169da4843d5b1a98c0384c2026
henrikwirth <sirhenry91@gmail.com>
2020-05-22 12:01:08 +0200

    update

--- tests/profiling.js
+++ tests/profiling.js
@@ -1,12 +1,15 @@
+const path = require('path');
+const fs = require('fs');
 
 let startUsage = process.cpuUsage()
 console.log("START:", process.pid, process.argv, startUsage)
 
+const filePath = path.join('/proc' , process.pid.toString(), 'stat');
+
+readFile(filePath)
 
 process.on('exit', () => {
   const endInfo = {pid: process.pid, argv: process.argv, cpu: process.cpuUsage()}
-  const fs = require('fs');
-  const path = require('path');
 
   const data = String(endInfo.cpu.user + endInfo.cpu.system)
 
@@ -14,3 +17,15 @@ process.on('exit', () => {
 
   console.log("END:", endInfo)
 });
+
+function readFile(filePath) {
+  console.log("FilePath-PID:", filePath)
+
+  fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
+    if (!err) {
+      console.log('received data: ' + data);
+    } else {
+      console.log(err);
+    }
+  });
+}
