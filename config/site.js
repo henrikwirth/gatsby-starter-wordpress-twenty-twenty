ed9ce8eda424cdc8cdafda857cee0fa2cf21aeb5
David Sapp <dsapp@Davids-iMac.local>
2020-11-24 03:28:26 -0700

    fix config example data

--- config/site.js
+++ config/site.js
@@ -3,22 +3,22 @@ module.exports = {
   title: 'Example Company', // Navigation and Site Title
   titleAlt: 'Example Company', // Title for JSONLD
   description: 'Example Company',
-  url: 'https://mobius.digital', // Domain of your site. No trailing slash!
-  siteUrl: 'https://mobius.digital', // url + pathPrefix
+  url: 'https://example.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://example.com', // url + pathPrefix
   blogUri: '/blog',
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'devsite', // shortname for manifest. MUST be shorter than 12 characters
+  shortName: 'example', // shortname for manifest. MUST be shorter than 12 characters
   author: 'Example Author', // Author for schemaORGJSONLD
   themeColor: '#ff6363',
   backgroundColor: '#ff6363',
   twitter: '', // Twitter Username
   // EXTRA
   company: 'Example Company LLC',
-  hoursOfOperation: '',
+  hoursOfOperation: 'M-F: 9am - 5pm',
   address: '123 Example St, Exampletown, USA',
   phone: '123.123.1234',
   email: 'me@example.com',
