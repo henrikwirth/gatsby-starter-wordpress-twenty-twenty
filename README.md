41f2a9017443e02ab48dfb2ac8dd21d05930a5ca
David Sapp <david@mobius.digital>
2021-01-18 19:01:01 -0700

    add netlify forms

--- README.md
+++ README.md
@@ -35,40 +35,6 @@ add_action( 'init', 'twentytwenty_menus' );
 -  Landing page
 -  Footer widget menus
 
-## Setup and Usage
-
--  Follow upstream repo instructions here: [@henrikwirth/gatsby-starter-wordpress-twenty-twenty](https://github.com/henrikwirth/gatsby-starter-wordpress-twenty-twenty)
--  Update WordPress Twenty Twenty theme `functions.php` menus
-
-```
-/**
- * Register navigation menus uses wp_nav_menu in five places.
- */
-function twentytwenty_menus() {
-
-	$locations = array(
-		'primary'  => __( 'Desktop Horizontal Menu', 'twentytwenty' ),
-		'expanded' => __( 'Desktop Expanded Menu', 'twentytwenty' ),
-		'mobile'   => __( 'Mobile Menu', 'twentytwenty' ),
-		'footer-1'   => __( 'Footer Menu 1', 'twentytwenty' ),
-		'footer-2'   => __( 'Footer Menu 2', 'twentytwenty' ),
-		'footer-3'   => __( 'Footer Menu 3', 'twentytwenty' ),
-		'footer-4'   => __( 'Footer Menu 4', 'twentytwenty' ),
-		'footer-5'   => __( 'Footer Menu 5', 'twentytwenty' ),
-		'social'   => __( 'Social Menu', 'twentytwenty' ),
-	);
-
-	register_nav_menus( $locations );
-}
-
-add_action( 'init', 'twentytwenty_menus' );
-```
-
-## Additions
-
--  Contact form
--  Landing page
-
 ## Limitations
 
 -  Comments are not implemented.
@@ -80,7 +46,6 @@ add_action( 'init', 'twentytwenty_menus' );
 -  Featured images
 -  Google Analytics
 -  Google Maps
--  Netlify forms
 -  Customizer
 
 ## Acknowledgements
---
+++ src/pages/contact/index.js
@@ -0,0 +1,121 @@
+import React from 'react'
+import { navigate } from 'gatsby-link'
+import Layout from '../../components/Layout'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isValidated: false }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  render() {
+    return (
+      <Layout
+        bodyClass={`page-template-default page has-no-pagination not-showing-comments footer-top-visible customize-support`}
+      >
+        <header className="blog-header has-text-align-center header-footer-group">
+          <div className="blog-header-inner section-inner medium">
+            <h1 className="entry-title">
+              Contact Us
+            </h1>
+          </div>
+        </header>
+        <div className="contact-page">
+          <div className="contact-form">
+            <form
+              name="contact"
+              method="post"
+              action="/contact/thanks/"
+              data-netlify="true"
+              data-netlify-honeypot="bot-field"
+              onSubmit={this.handleSubmit}
+            >
+              {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+              <input type="hidden" name="form-name" value="contact" />
+              <div hidden>
+                <label>
+                  Don’t fill this out:{' '}
+                  <input name="bot-field" onChange={this.handleChange} />
+                </label>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'name'}>
+                  Your name
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'text'}
+                    name={'name'}
+                    onChange={this.handleChange}
+                    id={'name'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'email'}>
+                  Email
+                </label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type={'email'}
+                    name={'email'}
+                    onChange={this.handleChange}
+                    id={'email'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <label className="label" htmlFor={'message'}>
+                  Message
+                </label>
+                <div className="control">
+                  <textarea
+                    className="textarea"
+                    name={'message'}
+                    onChange={this.handleChange}
+                    id={'message'}
+                    required={true}
+                  />
+                </div>
+              </div>
+              <div className="field">
+                <button className="button is-link special" type="submit">
+                  Send
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
